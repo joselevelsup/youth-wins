916fe3d33a113681a5973a8b31edb17698c5f23d
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MBP-2.T-mobile.com>
2018-08-08 14:07:01 -0400

    feat(Root): add routing logic based on login status

--- client/site/components/Root.js
+++ client/site/components/Root.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
 import Landing from './Landing'
 import Login from './Login'
 import Signup from './Signup'
+import Home from './Home'
 
-export default class Root extends Component {
+class Root extends Component {
+	componentDidMount(){
+		this.props.user.loggedIn ? this.props.history.push('/home') : null
+	}
+
 	render(){
 		return (
 			<Switch>
 				<Route exact path="/" component={Landing}/>
 				<Route exact path="/login" component={Login}/>
 				<Route exact path="/signup" component={Signup}/>
+				<Route exact path="/home" component={Home}/>
+				<Route component={Landing}/>
 			</Switch>
 		)
 	}
 }
+
+const mapStateToProps = state => state
+
+const RootComponent = withRouter(connect(mapStateToProps)(Root))
+export default RootComponent
