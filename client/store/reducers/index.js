1b2df5a34b9af7443444f297f8f145382c563102
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MBP-2.T-mobile.com>
2018-08-17 16:14:30 -0400

    feat(store): add form reducer to combineReducer

--- client/store/reducers/index.js
+++ client/store/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux'
 import user from './user'
 import  resources from "./resources";
+import { reducer as formReducer } from 'redux-form'
 
-export default combineReducers({user, resources});
+export default combineReducers({user, resources, form: formReducer});
