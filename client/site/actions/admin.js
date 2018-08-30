420e1143b79ea7542ee0b3fd0455bef215030ec5
joselevelsup <hipnotickid12@gmail.com>
2018-08-30 00:28:24 -0400

    fixing users and s3

--- client/site/actions/admin.js
+++ client/site/actions/admin.js
@@ -6,13 +6,21 @@ import {
     ADMIN_RES_S,
     ADMIN_USERS_S,
     ADMIN_APPS_S,
-    ADMIN_CREATE_USER,
-    ADMIN_FAILED_USER,
+    ADMIN_CREATE_STAFF_S,
+    ADMIN_CREATE_STAFF_F,
+    ADMIN_UPDATE_STAFF_S,
+    ADMIN_UPDATE_STAFF_F,
+    ADMIN_DELETE_STAFF_S,
+    ADMIN_DELETE_STAFF_F,
     CREATED_RES_S,
     CREATED_RES_F,
     UPDATED_RES_S,
     UPDATED_RES_F,
-    DELETED_RES_S
+    DELETED_RES_S,
+    APPROVED_RES_S,
+    APPROVED_RES_F,
+    DENIED_RES_S,
+    DENIED_RES_F
 } from "../constants/constants";
 
 export const adminSuccess = data => ({
@@ -110,6 +118,52 @@ export const deleteResource = id => ({
     }
 });
 
+export const approvedResource = data => ({
+    type: APPROVED_RES_S,
+    payload: data
+});
+
+export const failedToApprove = err => ({
+    type: APPROVED_RES_F,
+    payload: err
+});
+
+export const approve = id => ({
+    type: API,
+    payload: {
+        url: API_ADMIN+"/resource/approve",
+        method: "POST",
+        data: {
+            resourceId: id
+        },
+        success: approvedResource,
+        error: failedToApprove
+    }
+});
+
+export const deniedResource = data => ({
+    type: DENIED_RES_S,
+    payload: data
+});
+
+export const failedToDeny = err => ({
+    type: DENIED_RES_F,
+    payload: err
+});
+
+export const deny = id => ({
+    type: API,
+    payload: {
+        url: API_ADMIN+"/resource/deny",
+        method: "POST",
+        data: {
+            resourceId: id
+        },
+        success: deniedResource,
+        error: failedToDeny
+    }
+});
+
 export const allUsers = data => ({
     type: ADMIN_USERS_S,
     payload: data
@@ -123,28 +177,73 @@ export const getAllUsers = () => ({
     }
 });
 
-export const createdUser = data => ({
-    type: ADMIN_CREATE_USER,
+export const createdStaff = data => ({
+    type: ADMIN_CREATE_STAFF_S,
     payload: data
 });
 
-export const failedCreateUser = err => ({
-    type: ADMIN_FAILED_USER,
+export const failedCreateStaff = err => ({
+    type: ADMIN_CREATE_STAFF_F,
     payload: err
 });
 
-export const createUser = (userData) => ({
+export const createStaff = (userData) => ({
     type: API,
     payload: {
-        url: API_ADMIN+`/users`,
+        url: API_ADMIN+`/users/s`,
+        method: "POST",
+        data: userData,
+        success: createdStaff,
+        error: failedCreateStaff
+    }
+});
+
+export const updatedStaff = data => ({
+    type: ADMIN_UPDATE_STAFF_S,
+    payload: data
+});
+
+export const failedUpdateStaff = err => ({
+    type: ADMIN_UPDATE_STAFF_F,
+    payload: err
+});
+
+export const updateStaff = (id, userData) => ({
+    type: API,
+    payload: {
+        url: API_ADMIN+`/users/s`,
         method: "PUT",
         data: {
+            resourceId: id,
             user: userData
         },
-        success: createdUser,
-        error: failedCreateUser
+        success: updatedStaff,
+        error: failedUpdateStaff
     }
-})
+});
+
+export const deletedStaff = data => ({
+    type: ADMIN_DELETE_STAFF_S,
+    payload: data
+});
+
+export const failedDeleteStaff = err => ({
+    type: ADMIN_DELETE_STAFF_F,
+    payload: err
+});
+
+export const deleteStaff = (id, userData) => ({
+    type: API,
+    payload: {
+        url: API_ADMIN+`/users/s`,
+        method: "DELETE",
+        data: {
+            resourceId: id
+        },
+        success: deletedStaff,
+        error: failedDeleteStaff
+    }
+});
 
 export const allApps = data => ({
     type: ADMIN_APPS_S,
--- client/site/components/modal.js
+++ client/site/components/modal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Field } from "redux-form";
 import {
     Button,
     Modal,
@@ -17,7 +18,7 @@ export const YouthModal = ({ open, toggle }) => (
         </Modal>
 );
 
-export const ResourceModal = ({ open, toggle, resource, apply, admin }) => (
+export const ResourceModal = ({ open, toggle, resource, apply, admin, edit, remove, approve, deny }) => (
     <Modal isOpen={open} toggle={toggle}>
         <div className="modal-header">
             <Container fluid={true}>
@@ -60,19 +61,19 @@ export const ResourceModal = ({ open, toggle, resource, apply, admin }) => (
                     resource.pending ?
                     <ModalFooter>
                         <Col md={{size: 4, offset: 4}}>
-                            <Button block color="primary">Approve</Button>
+                          <Button onClick={approve} block color="primary">Approve</Button>
                         </Col>
                         <Col md={4}>
-                            <Button block color="secondary">Deny</Button>
+                          <Button onClick={deny} block color="secondary">Deny</Button>
                         </Col>
                     </ModalFooter>
                     :
                     <ModalFooter>
                         <Col md={{size: 4, offset: 4}}>
-                            <Button block color="primary">Edit</Button>
+                          <Button onClick={edit} block color="primary">Edit</Button>
                         </Col>
                         <Col md={4}>
-                            <Button block color="secondary">Remove</Button>
+                          <Button onClick={remove} block color="secondary">Remove</Button>
                         </Col>
                     </ModalFooter>
                     )
@@ -139,5 +140,153 @@ export const UserModal = ({ open, toggle, user }) => (
         <ModalBody>
             
         </ModalBody>
+    </Modal>
+)
+
+export const CreateResource = ({ open, toggle, create }) => (
+    <Modal isOpen={open} toggle={toggle}>
+      <ModalBody>
+        <main className="form-wrap">
+          <form onSubmit={create} className="support-form">
+					  <div className="form-group support-form-items">
+						  <label>Name</label>
+						  <Field name="orgName" className="form-control" component="input" type="text" />
+					  </div>
+            <div className="form-group support-form-items">
+						  <label>Website</label>
+						  <Field name="website" className="form-control" component="input" type="text" />
+					  </div>
+            <div className="form-group support-form-items">
+						  <label>Contact Email</label>
+						  <Field name="contactEmail" className="form-control" component="input" type="text" />
+					  </div>
+            <div className="form-group support-form-items">
+						  <label>Description</label>
+						  <Field name="description" className="form-control" component="input" type="text" />
+					  </div>
+					  {/* <FormGroup className="support-form-items">
+						   <Label>Logo</Label><br/>
+						   <div className="tallInput resourceUploadImage">
+							 <Button className="uploadButton" color="warning" onClick={this.fileUpload}>+</Button>
+						   </div>						
+						   <input ref={e => this.uploader = e} className="fileHidden" name="logo" type="file"/>
+					     </FormGroup> */}
+					  <Button color="warning" type="submit">submit</Button>
+          </form>
+        </main>
+      </ModalBody>
+    </Modal>
+)
+
+export const EditResource = ({ open, toggle, resource }) => (
+    <Modal isOpen={open} toggle={toggle}>
+      <div className="modal-header">
+        <Container fluid={true}>
+          <Row>
+            <Col md={{size: 1, offset: 11}}>
+              <Button color="clear" onClick={toggle}>X</Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <ModalBody>
+        <main className="form-wrap">
+          <form className="support-form">
+					  <div className="form-group support-form-items">
+						  <label>Organization Name</label>
+						  <Field name="orgName" className="form-control" component="input" type="text" />
+					  </div>
+            <div className="form-group support-form-items">
+						  <label>Website</label>
+						  <Field name="website" className="form-control" component="input" type="text" />
+					  </div>
+            <div className="form-group support-form-items">
+						  <label>Contact Email</label>
+						  <Field name="contactEmail" className="form-control" component="input" type="text" />
+					  </div>
+            <div className="form-group support-form-items">
+						  <label>Description</label>
+						  <Field name="description" className="form-control" component="input" type="text" />
+					  </div>
+					  {/* <FormGroup className="support-form-items">
+						   <Label>Logo</Label><br/>
+						   <div className="tallInput resourceUploadImage">
+							 <Button className="uploadButton" color="warning" onClick={this.fileUpload}>+</Button>
+						   </div>						
+						   <input ref={e => this.uploader = e} className="fileHidden" name="logo" type="file"/>
+					     </FormGroup> */}
+					  <Button color="warning" type="submit">submit</Button>
+          </form>
+        </main>
+      </ModalBody>
+    </Modal>
+)
+
+
+export const CreateStaff = ({ open, toggle, create }) => (
+    <Modal isOpen={open} toggle={toggle} size="lg">
+      <div className="modal-header">
+        <Container fluid={true}>
+          <Row>
+            <Col md={{size: 1, offset: 11}}>
+              <Button color="clear" onClick={toggle}>X</Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <ModalBody>
+          <form onSubmit={create}>
+            <div className="row">
+              <div className="col-4">
+                <br />
+              </div>
+              <div className="col-8">
+                <div className="row">
+                  <div className="col-6">
+					          <div className="form-group">
+						          <label>First Name</label>
+						          <Field name="firstName" className="form-control" component="input" type="text" />
+					          </div>
+                  </div>
+                  <div className="col-6">
+					          <div className="form-group">
+						          <label>Position</label>
+						          <Field name="position" className="form-control" component="input" type="text" />
+					          </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-6">
+					          <div className="form-group">
+						          <label>Last Name</label>
+						          <Field name="lastName" className="form-control" component="input" type="text" />
+					          </div>
+                  </div>
+                  <div className="col-6">
+					          <div className="form-group">
+						          <label>Password</label>
+						          <Field name="password" className="form-control" component="input" type="password" />
+					          </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-6">
+					          <div className="form-group">
+						          <label>Email</label>
+						          <Field name="email" className="form-control" component="input" type="text" />
+					          </div>
+                  </div>
+                  <div className="col-6">
+					          <div className="form-group">
+						          <label>Confirm Password</label>
+						          <Field name="cpassword" className="form-control" component="input" type="password" />
+					          </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+					  <Button color="warning" type="submit">submit</Button>
+          </form>
+      </ModalBody>
     </Modal>
 )
